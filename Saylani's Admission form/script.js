a574de04cdeda339bc3bdf87c692b3d280d551ce
rafay45 <rafayy579@gmail.com>
2025-07-05 22:12:31 +0500

    📑📚Saylani's Registration Form

---
+++ Saylani's Admission form/script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCmifs89jRdGoZgoke8URa9tibVuEQGBk",
+  authDomain: "saylani-s-registration-form.firebaseapp.com",
+  projectId: "saylani-s-registration-form",
+  storageBucket: "saylani-s-registration-form.firebasestorage.app",
+  messagingSenderId: "1078821106443",
+  appId: "1:1078821106443:web:e081c3909ca989ee1b680e",
+  measurementId: "G-2ZYV5Z538W"
+};
