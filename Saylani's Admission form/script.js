ff71a4054f61c8c64ab2db3069d7a29a65958168
rafay45 <rafayy579@gmail.com>
2025-07-05 23:27:49 +0500

    📑📚Saylani's Registration form

--- Saylani's Admission form/script.js
+++ Saylani's Admission form/script.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: "AIzaSyDCmifs89jRdGoZgoke8URa9tibVuEQGBk",
   authDomain: "saylani-s-registration-form.firebaseapp.com",
@@ -8,3 +9,20 @@ const firebaseConfig = {
   appId: "1:1078821106443:web:e081c3909ca989ee1b680e",
   measurementId: "G-2ZYV5Z538W"
 };
+
+const app = initializeApp(firebaseConfig)
+
+const getStore = getFirestore(app)
+
+async function addInfo() {
+  try {
+    await addDoc(collection(getStore, "users"), {
+      
+    })
+  } catch (error) {
+    console.log("Error is getStore :: ", error);
+    
+  }
+}
+
+
